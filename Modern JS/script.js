8c8b2ac29480a2f2b2b9aff3f185601dabb2dea4
defford <defford@gmail.com>
2024-07-12 10:08:48 -0230

    Added files to main branch

---
+++ Modern JS/script.js
@@ -0,0 +1,33 @@
+let x;
+
+x = 1;
+
+
+
+
+
+x = 2 === '2';
+
+x = 2 !== '2';
+
+x = 10 > 5;
+
+x = null;
+
+
+const myString = 'developer';
+
+let substring = myString.;
+
+let spliceString = myString.padEnd(15, "-")
+let sString = myString.log()
+
+let string = myString.charAt(2).toUpperCase();
+
+x = 50;
+
+y = x.
+
+console.log(sString)
+
+// console.log(x, typeof x); // Output: 1
